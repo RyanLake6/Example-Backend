6d29ec74de36e03207c66879538ecb8ed373d2ec
Ryan Lake <73082037+RyanLake6@users.noreply.github.com>
2023-02-03 15:40:58 -0500

    sql instance ported over to local hosting

--- database/database_connect.go
+++ database/database_connect.go
@@ -1,60 +1,44 @@
 package database
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
-	"net"
+	"log"
 	"os"
 
-	"cloud.google.com/go/cloudsqlconn"
 	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 type Database struct {
 	DB *sql.DB
 }
 
-func ConnectWithConnector(DB_USER *string, DB_PASS *string, DB_NAME *string, INSTANCE_CONNECTION_NAME *string) (*Database, error) {
+func DatabaseConnect() (*Database, error) {
 	var database Database
+	var err error
 
-	// Checking in env variables aren't properly loaded in
-	if *DB_USER == "" || *DB_PASS == "" || *DB_NAME == "" || *INSTANCE_CONNECTION_NAME == "" {
-		return nil, fmt.Errorf("Couldn't load env variables")
+	godotenv.Load()
+	// Capture connection properties.
+	cfg := mysql.Config{
+		ParseTime: true,
+		User:      os.Getenv("DBUSER"),
+		Passwd:    os.Getenv("DBPASS"),
+		Net:       "tcp",
+		Addr:      "127.0.0.1:3306",
+		DBName:    "household-db",
 	}
-
-	var (
-		dbUser                 = DB_USER                  // e.g. 'my-db-user'
-		dbPwd                  = DB_PASS                  // e.g. 'my-db-password'
-		dbName                 = DB_NAME                  // e.g. 'my-database'
-		instanceConnectionName = INSTANCE_CONNECTION_NAME // e.g.  maybe /cloudql/ 'project:region:instance'
-		usePrivate             = os.Getenv("PRIVATE_IP")
-	)
-
-	d, err := cloudsqlconn.NewDialer(context.Background())
+	// Get a database handle.
+	database.DB, err = sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
-		return nil, fmt.Errorf("cloudsqlconn.NewDialer: %v", err)
-	}
-	var opts []cloudsqlconn.DialOption
-	// cloud sql is setup on public IP this will never be hit unless this is changed
-	if usePrivate != "" {
-		opts = append(opts, cloudsqlconn.WithPrivateIP())
+		return nil, err
 	}
 
-	// Connecting to cloud sql database
-	mysql.RegisterDialContext("cloudsqlconn",
-		func(ctx context.Context, addr string) (net.Conn, error) {
-			return d.Dial(ctx, *instanceConnectionName, opts...)
-		})
-
-	dbURI := fmt.Sprintf("%s:%s@cloudsqlconn(localhost:3306)/%s?parseTime=true",
-		*dbUser, *dbPwd, *dbName)
-
-	// Opening mysql database
-	database.DB, err = sql.Open("mysql", dbURI)
-	if err != nil {
-		return nil, fmt.Errorf("sql.Open: %v", err)
+	pingErr := database.DB.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
 	}
+	fmt.Println("Connected to sql!")
 	return &database, nil
 }
 
@@ -65,7 +49,7 @@ type User struct {
 }
 
 func (d *Database) GetUser() ([]User, error) {
-	rows, err := d.DB.Query("SELECT * FROM User")
+	rows, err := d.DB.Query("SELECT * FROM user")
 	if err != nil {
 		return nil, fmt.Errorf("error running query: %w", err)
 		//gctx.Status(http.StatusInternalServerError)
--- src/main.go
+++ src/main.go
@@ -5,22 +5,19 @@ import (
 	"backend/database"
 	"backend/logic"
 	"log"
-
-	"github.com/joho/godotenv"
-	"github.com/namsral/flag"
 )
 
 func main() {
 	// Loading in all the environment variables
-	godotenv.Load("./.env")
-	DB_USER := flag.String("DB_USER", "", "username for db connecting/authentication")
-	DB_PASS := flag.String("DB_PASS", "", "Password for db connecting/authentication")
-	DB_NAME := flag.String("DB_NAME", "", "Name of db to connect to")
-	INSTANCE_CONNECTION_NAME := flag.String("INSTANCE_CONNECTION_NAME", "", "Instance ID of cloud sql")
-	flag.Parse()
+	// godotenv.Load("./.env")
+	// DB_USER := flag.String("DB_USER", "", "username for db connecting/authentication")
+	// DB_PASS := flag.String("DB_PASS", "", "Password for db connecting/authentication")
+	// DB_NAME := flag.String("DB_NAME", "", "Name of db to connect to")
+	// INSTANCE_CONNECTION_NAME := flag.String("INSTANCE_CONNECTION_NAME", "", "Instance ID of cloud sql")
+	// flag.Parse()
 
 	// setting up database connection
-	db, err := database.ConnectWithConnector(DB_USER, DB_PASS, DB_NAME, INSTANCE_CONNECTION_NAME)
+	db, err := database.DatabaseConnect()
 	if err != nil {
 		log.Fatal("Error connecting database: ", err)
 	}
